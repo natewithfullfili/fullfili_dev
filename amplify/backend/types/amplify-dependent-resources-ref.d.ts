329fbec05efaf688ecc168d99bdd345efd4e0d66
Nate Pedersen <nate@fullfili.com>
2023-06-12 13:48:54 -0600

    Auth Added

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,13 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "fullfiliprodbb941ab2": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
+import { Amplify } from 'aws-amplify';
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+import awsExports from './aws-exports';
+
+Amplify.configure(awsExports);
 
 function App() {
   return (
-    <div className="App">
-      <h1>Welcome to Fullfili</h1>
-    </div>
+    <Authenticator>
+      <div className="App">
+        <h1>Welcome to Fullfili</h1>
+      </div>
+    </Authenticator>
   );
 }
 
